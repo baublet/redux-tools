3904e1b39fc4c52bfcecc3d541f4a521a1d988db
Ryan M. Poe <rmp23@duke.edu>
2018-05-14 21:20:07 +0000

    Add extra test to ensure key naming works

--- src/reducers/clearEntityArray.test.js
+++ src/reducers/clearEntityArray.test.js
@@ -8,4 +8,14 @@ test('clears the entity array', () => {
         newState = reducer(testState)
         
     expect(newState.entities.length == 0)
-});
+})
+
+test('clears the entity array with specific name', () => {
+    const reducer = clearEntityArray("products"),
+        testState = {
+            products: [1, 2, 3]
+        },
+        newState = reducer(testState)
+        
+    expect(newState.products.length == 0)
+})
