dfb669158efd34f959f0d95654f1ee11a86fe69c
Ryan M. Poe <rmp23@duke.edu>
2018-05-30 18:36:07 +0000

    Add updateEntityPropMap and test

---
+++ src/reducers/updateEntityPropMap.js
@@ -0,0 +1,16 @@
+export default function updateEntityPropMap(
+    entityProp,
+    actionPayloadEntityIdentifier = "id",
+    stateEntitiesProp = "entities",
+    actionPayloadProp = "value"
+) {
+    return (state, action) => {
+        const newValue = action.payload[actionPayloadProp],
+            entityIdToUpdate = action.payload[actionPayloadEntityIdentifier],
+            updatedEntities = Object.assign({}, state[stateEntitiesProp])
+        
+        updatedEntities[entityIdToUpdate][entityProp] = newValue
+
+        return Object.assign({}, state, { [stateEntitiesProp]: updatedEntities })
+    }
+}
