b4afa8c3dac60bfba9c08a46b54a3ef3861c3251
Ryan M. Poe <rmp23@duke.edu>
2018-05-30 18:28:30 +0000

    Add updateEntityPropArray and test

---
+++ src/reducers/updateEntityPropArray.js
@@ -0,0 +1,20 @@
+export default function updateEntityPropArray(
+    entityProp,
+    actionPayloadEntityIdentifier = "id",
+    stateEntitiesProp = "entities",
+    actionPayloadProp = "value",
+    entityIdentifier = "id"
+) {
+    return (state, action) => {
+        const newValue = action.payload[actionPayloadProp],
+            entityIdToUpdate = action.payload[actionPayloadEntityIdentifier],
+            updatedEntities = state[stateEntitiesProp].map(entity => {
+                if(entity[entityIdentifier] == entityIdToUpdate) {
+                    entity[entityProp] = newValue
+                }
+                return entity
+            })
+
+        return Object.assign({}, state, { [stateEntitiesProp]: updatedEntities })
+    }
+}
---
+++ src/reducers/updateEntityPropArray.test.js
@@ -0,0 +1,41 @@
+import updateEntityPropArray from "./updateEntityPropArray"
+
+function state() {
+    return {
+        entities: [{
+            name: "Test",
+            id: 1
+        }, {
+            name: "Second Test",
+            id: 2
+        }]
+    }
+}
+
+test('updates individual entity props', () => {
+    const reducer = updateEntityPropArray("name"),
+        testState = state(),
+        newState = reducer(testState, {
+            payload: {
+                id: 1,
+                value: "Renamed Test",
+            }
+        })
+    expect(newState.entities.length).toBe(2)
+    expect(newState.entities[0].name).toBe("Renamed Test")
+    expect(newState.entities[1].name).toBe("Second Test")
+})
+
+test('updates individual entity props using alternative prop names', () => {
+    const reducer = updateEntityPropArray("name", "ident"),
+        testState = state(),
+        newState = reducer(testState, {
+            payload: {
+                ident: 1,
+                value: "Renamed Test",
+            }
+        })
+    expect(newState.entities.length).toBe(2)
+    expect(newState.entities[0].name).toBe("Renamed Test")
+    expect(newState.entities[1].name).toBe("Second Test")
+})
