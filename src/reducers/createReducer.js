59665e8ba4e494d687525e4df069694668217a50
Ryan Poe <baublet@gmail.com>
2018-11-27 09:57:24 -0600

    Fix potential bug and upate jest/deps to work with new config

--- src/reducers/createReducer.js
+++ src/reducers/createReducer.js
@@ -1,6 +1,6 @@
 export default function createReducer(initialState, handlers) {
     return (state = initialState, action) => {
-        if (handlers.hasOwnProperty(action.type)) {
+        if (handlers && handlers.hasOwnProperty && handlers.hasOwnProperty(action.type)) {
             return handlers[action.type](state, action)
         }
         return state
